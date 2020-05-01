7dd05e1987fb10a73bc9eade8858d18133f164b4
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2020-05-01 13:47:39 +0530

    refs #master employee edit and delete functions completed

--- EmployeeManager/Controllers/HomeController.cs
+++ EmployeeManager/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 using EmployeeManager.Models;
 using EmployeeManager.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Hosting.Internal;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace EmployeeManager.Controllers
@@ -11,10 +16,12 @@ namespace EmployeeManager.Controllers
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.webHostEnvironment = webHostEnvironment;
         }
         public ViewResult Index()
         {
@@ -45,11 +52,21 @@ namespace EmployeeManager.Controllers
         {
             if (ModelState.IsValid)
             {
+                string uniqueFileName = null;
+                if (model.Photo != null)
+                {
+                    string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
                 Employee employee = new Employee
                 {
                     Name = model.Name,
                     Email = model.Email,
-                    Department = model.Department
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
                 };
 
                 _employeeRepository.Add(employee);
@@ -59,17 +76,62 @@ namespace EmployeeManager.Controllers
         }
 
         [HttpGet]
-        public ViewResult Edit(int? id)
+        public ViewResult Edit(int id)
         {
-            var employee = _employeeRepository.GetEmployee(id??1);
-            return View("create", employee);
+            var employee = _employeeRepository.GetEmployee(id);
+
+            if (employee == null)
+            {
+                return View("index");
+            }
+
+            var editEmployee = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+
+            return View("edit", editEmployee);
         }
 
         [HttpPost]
-        public RedirectToActionResult Edit(Employee employee)
+        public IActionResult Edit(EmployeeEditViewModel editEmployee)
         {
-            _employeeRepository.Edit(employee);
-            return RedirectToAction("details", new {id = employee.Id});
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+                if (editEmployee.Photo != null)
+                {
+                    string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + editEmployee.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    editEmployee.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee employee = new Employee
+                {
+                    Id = editEmployee.Id,
+                    Name = editEmployee.Name,
+                    Email = editEmployee.Email,
+                    Department = editEmployee.Department,
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Edit(employee);
+                return RedirectToAction("details", new { id = employee.Id });
+            }
+
+            return View(editEmployee);
+        }
+
+        [HttpGet]
+        public RedirectToActionResult Delete(int id)
+        {
+            _employeeRepository.Delete(id);
+            return RedirectToAction("index");
         }
     }
 }
